d5876e1d738d8e3074fbca51ad7c939a3cfb8c58
Slipp Douglas Thompson <git@capnslipp.com>
2022-12-18 05:50:21 -0600

    Converted `NSPreferencePane` subclass `AdditionalAccentColors` to Swift
    * Converted `NSPreferencePane` subclass `AdditionalAccentColors` to Swift.
    * Moved moved the `SWIFT_VERSION` build setting up to project-level.
    
    Wrote some test code to make sure it was working and able to update a NSTextField in the view.  It works!  (Not checking in those testing changes.)

---
+++ AdditionalAccentColors/AdditionalAccentColors.swift
@@ -0,0 +1,32 @@
+//
+//  AdditionalAccentColors.swift
+//  AdditionalAccentColors
+//
+//  Created by Cap'n Slipp on 12/18/22.
+//
+
+import PreferencePanes
+
+class AdditionalAccentColors : NSPreferencePane {
+
+	@IBOutlet weak var testTextField: NSTextField!
+	
+	override func mainViewDidLoad()
+	{
+		self.testTextField.stringValue = "It worked!"
+		
+		let alert = NSAlert()
+		alert.messageText = "This is the message text"
+		alert.informativeText = "This is the informative text"
+		alert.addButton(withTitle: "OK")
+		alert.addButton(withTitle: "Cancel")
+
+		let response = alert.runModal()
+		if response == .alertFirstButtonReturn {
+			print("OK button clicked")
+		} else {
+			print("Cancel button clicked")
+		}
+	}
+	
+}
