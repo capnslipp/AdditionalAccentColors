3eb8e40136b4b2c3823ac9eaf6bbcc0f718115cf
Slipp Douglas Thompson <git@capnslipp.com>
2022-12-18 05:08:25 -0600

    Initial Commit— project with Preference Pane target

---
+++ AdditionalAccentColors/AdditionalAccentColors.m
@@ -0,0 +1,16 @@
+//
+//  AdditionalAccentColors.m
+//  AdditionalAccentColors
+//
+//  Created by Cap'n Slipp on 12/18/22.
+//
+
+#import "AdditionalAccentColors.h"
+
+@implementation AdditionalAccentColors
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
